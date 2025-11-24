f4b42ea721fa02c5c08d52fc0967791dad01423f
rob587 <robertocamm02@yahoo.com>
2025-11-24 14:50:12 +0100

    input collegati

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,64 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  return <></>;
+  const [infos, setInfos] = useState({
+    nome: "",
+    username: "",
+    password: "",
+    spec: "",
+    experience: "",
+    description: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInfos((prevInfos) => ({
+      ...prevInfos,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        name="nome"
+        value={infos.nome}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="username"
+        value={infos.username}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        value={infos.password}
+        onChange={handleChange}
+      />
+      <select name="spec" id="" value={infos.spec} onChange={handleChange}>
+        <option value="">Seleziona una specializzazione</option>{" "}
+        <option value="fullstack">Full stack</option>
+        <option value="frontend">Front end</option>
+        <option value="backend">Backend</option>
+      </select>
+      <input
+        type="number"
+        name="experience"
+        value={infos.experience}
+        onChange={handleChange}
+      />
+      <textarea
+        name="description"
+        value={infos.description}
+        onChange={handleChange}
+      />
+
+      <button>Submit</button>
+    </>
+  );
 }
 
 export default App;
